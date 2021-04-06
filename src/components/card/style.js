efedebb025299dd08086eaf112bf96ff435e13d2
Alezuzzo <Thallesander@hotmail.com>
2021-04-05 22:35:52 -0300

    mudanças css de componentes

--- src/components/card/style.js
+++ src/components/card/style.js
@@ -2,10 +2,10 @@ import styled from 'styled-components';
 
 export const CardContainer = styled.div`
 height: 300px;
-width: 200px;
-border: 1px solid #ccc;
+width: 250px;
+border: 2px solid #FEAA2D;
 border-radius: 40px;
-margin: 10px;
+margin: 6px;
 `
 export const ImageContainer = styled.img`
 height: 200px;
@@ -13,6 +13,12 @@ width: 200px;
 `
 
 export const NamePoke = styled.h1`
-font-size: 16px;
+width: 244px;
+border: 4px solid #FEAA2D;
+background-color: #FEAA2D;
+font-size: 30px;
+font-family: 'Helvetica';
 color: white;
+padding-bottom: 36px;
+border-radius: 0px 0px 40px 40px;
 `
